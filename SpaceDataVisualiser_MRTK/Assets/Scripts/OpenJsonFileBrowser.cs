5603f623ee8cf7c0e0c386c4e7e6fad380da3a97
Ceris Brown <james@impromptu-software.co.uk>
2021-01-06 19:23:06 +0000

    Reorganise scene and add file open dialog to hololens

--- SpaceDataVisualiser_MRTK/Assets/Scripts/OpenJsonFileBrowser.cs
+++ SpaceDataVisualiser_MRTK/Assets/Scripts/OpenJsonFileBrowser.cs
@@ -5,6 +5,16 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using SFB;
 
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+#if ENABLE_WINMD_SUPPORT
+using Windows.Storage.Pickers;
+using Windows.Storage;
+#endif
+
 [RequireComponent(typeof(Button))]
 public class OpenJsonFileBrowser : MonoBehaviour, IPointerDownHandler {
 
@@ -20,10 +30,60 @@ public class OpenJsonFileBrowser : MonoBehaviour, IPointerDownHandler {
         button.onClick.AddListener(onClick);
 	}
 	
-	private void onClick()
+	private async void onClick()
     {
+#if ENABLE_WINMD_SUPPORT
+        // Windows FileOpenPicker Implementation:
+
+        string FilePath = await LoadFileAsync();
+        Debug.Log(FilePath);
+        JDI.localpath = FilePath;
+#else
+        // Standalone File Browser Implementation:
         var paths = StandaloneFileBrowser.OpenFilePanel("Title", "", "json", false);
         string FileLocation = new System.Uri(paths[0]).AbsoluteUri;
         JDI.localpath = FileLocation;
+#endif
+    }
+
+
+    internal static async Task<string> LoadFileAsync()
+    {
+        {
+#if ENABLE_WINMD_SUPPORT
+            var pickCompleted = new TaskCompletionSource<string>();
+
+            UnityEngine.WSA.Application.InvokeOnUIThread(
+                async () =>
+                {
+                    Stream stream = null;
+                    FileOpenPicker picker = new FileOpenPicker();
+                    picker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+                    picker.FileTypeFilter.Add(".json");
+                    picker.FileTypeFilter.Add("*");
+                    picker.ViewMode = PickerViewMode.Thumbnail;
+                    picker.CommitButtonText = "Select Data";
+
+                    var file = await picker.PickSingleFileAsync();
+                    string filePath = null;
+
+                    if (file != null)
+                    {
+                        filePath = file.Path;
+                    }
+                    pickCompleted.SetResult(filePath);
+                },
+                true
+            );
+
+            await pickCompleted.Task;
+
+            return (pickCompleted.Task.Result);
+
+#else
+        throw new InvalidOperationException(
+            "Sorry, no file dialog support for other platforms here");
+#endif
+        }
     }
 }
